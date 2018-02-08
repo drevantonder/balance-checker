b70d5f5dd73477059a842a5eb7d68ad8fa77ec80
Andre van Tonder <drevtonder@gmail.com>
2018-02-08 11:58:30 +0800

    Added getting wallet adress from user

--- renderer.js
+++ renderer.js
@@ -1,11 +1,15 @@
 const axios = require('axios');
 const Vue = require("./node_modules/vue/dist/vue.js");
 
+const walletRegex = /^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$/;
+
 var balanceChecker = new Vue({
   el: '#balance-checker',
   data: {
     currency: "AUD", //TODO: add setting where user can set this
     exchangeRate: 0,
+    walletAddress: "1AvyuTeXNVqu1wJoGBYQwGkZbGJZtSs22E",
+    canUpdate: false,
 
     pools: [ //TODO: add setting where user can set this
       {"name":"aHashPool",   "API":"http://www.ahashpool.com/api/wallet?address=WALLET_ADDRESS",    "unpaidBalance": 0,"unpaidBalanceWorth": 0},
@@ -21,14 +25,24 @@ var balanceChecker = new Vue({
         }.bind(this));
     },
 
+    validateWalletAddress: function(){
+      if (walletRegex.test(this.walletAddress)){
+        this.canUpdate = true;
+        this.updatePools();
+      }
+    },
+
     updatePools: function () {
-      this.pools.forEach(function(pool){
-        this.getPoolData(pool);
-      }.bind(this));
+      if(this.canUpdate){
+        this.pools.forEach(function(pool){
+          this.getPoolData(pool);
+        }.bind(this));
+      }
+
     },
 
     getPoolData: function(pool){
-      var apiURL = pool.API.replace("WALLET_ADDRESS","1AvyuTeXNVqu1wJoGBYQwGkZbGJZtSs22E") //TODO: get the WALLET_ADDRESS from the input
+      var apiURL = pool.API.replace("WALLET_ADDRESS",this.walletAddress) //TODO: get the WALLET_ADDRESS from the input
       axios.get(apiURL)
         .then(function (response) {
           pool.unpaidBalance = (response.data.total_unpaid || response.data.unpaid);
@@ -38,15 +52,16 @@ var balanceChecker = new Vue({
           console.log("pool:",pool.name,"failed:",error)
           this.getPoolData(pool);
         }.bind(this));
-    }
+    },
 
     calculateWorth: function(cryptocurrencyAmount){
       return (cryptocurrencyAmount * this.exchangeRate).toFixed(2);
-    },
+    }
   },
 
   mounted: function () {
     this.setExchangeRate();
+    this.validateWalletAddress();
     this.updatePools();
 
     setInterval(function () {
